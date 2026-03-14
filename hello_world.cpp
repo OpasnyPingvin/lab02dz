29f41b57d78da668346c62ab180896199e82f1f6
OpasnyPingvin <ivananisimov2121@gmail.com>
2026-03-14 17:44:26 +0000

    Add comments to hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
+//main functin
 int main() {
-std::string name;
+std::string name; //var for user name
 std::cout << "Enter your name: ";
-std::cin >> name;
-std::cout << "Hello world form " << name << std::endl;
+std::cin >> name; //getting name
+std::cout << "Hello world form " << name << std::endl; //greeting
 return 0;
 }
